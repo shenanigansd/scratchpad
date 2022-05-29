3a8421f60c1868cf5215e3dd2f64d46ad520ab40
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-05-28 23:12:36 -0500

    haxe: remove invalid code

--- scratch/learning/languages/haxe/Main.hx
+++ scratch/learning/languages/haxe/Main.hx
@@ -5,8 +5,6 @@ class Main {
 	static public function main():Void {
 		// Single line comment
 		trace("Hello, trace!");
-		info("info");
-		error("error");
 		Sys.println("Hello, println!");
 		Sys.print("Hello, print!"); // no added newline
 		Sys.stderr().writeString("Yow!\n");
