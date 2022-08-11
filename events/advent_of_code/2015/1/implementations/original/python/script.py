9183aea14ed28ae0c1647807fd86c2755bdff95b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-08-10 19:20:26 -0500

    aoc/2015/1
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ events/advent_of_code/2015/1/implementations/original/python/script.py
@@ -0,0 +1,22 @@
+def part_one(text: str) -> int:
+    return sum(1 if char == "(" else -1 for char in text)
+
+
+def part_two(text: str) -> int:
+    index = 0
+    floor = 0
+    for char in text:
+        index += 1
+        if char == "(":
+            floor += 1
+        elif char == ")":
+            floor -= 1
+        if floor == -1:
+            return index
+
+
+if __name__ == '__main__':
+    data: str = open("../../../input.txt").readlines()[0].strip()
+
+    print(part_one(text=data))
+    print(part_two(text=data))
