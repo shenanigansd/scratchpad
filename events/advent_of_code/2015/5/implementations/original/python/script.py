0206b21654a145ec1ca27979f5eec318e722bb19
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-08-25 19:56:15 -0500

    aoc/2015/5-6
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ events/advent_of_code/2015/5/implementations/original/python/script.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+import re
+
+pat = r"(?=.*?(?:(.).\1))(?=.*?(.{2}).*?\2).*"
+
+
+def _contains_any(text: str, values: list[str]) -> bool:
+    return any(value in text for value in values)
+
+
+def _contains_at_least_x_of(text: str, occurrences: int, values: list[str]) -> bool:
+    return sum(text.count(value) for value in values) >= occurrences
+
+
+def _contains_sequential_duplicate(text: str) -> bool:
+    for current, nxt in zip(text, text[1:]):
+        if current == nxt:
+            return True
+    return False
+
+
+def _contains_duplicate_with_buffer(text: str, buffer: int = 1) -> bool:
+    for current, nxt in zip(text, text[buffer + 1:]):
+        if current == nxt:
+            return True
+    return False
+
+
+def _contains_non_overlapping_duplicate_sequence(text: str, length: int = 2) -> bool:
+    for index in range(len(text)):
+        if text.count(text[index:length + 1]) > 1 and len(text[index:length + 1].strip())>1:
+            print(f"{text[index:length + 1]}\t{text.count(text[index:length + 1])}")
+            return True
+    return False
+
+
+def part_one(values: list[str]) -> int:
+    nice_strings = 0
+    for value in values:
+        conditions = (
+            _contains_at_least_x_of(value, 3, list("aeiou")),
+            _contains_sequential_duplicate(value),
+            not _contains_any(value, ["ab", "cd", "pq", "xy"])
+        )
+        if all(conditions):
+            nice_strings += 1
+
+    return nice_strings
+
+
+def part_two(values: list[str]) -> int:
+    nice_strings = 0
+    for value in values:
+        conditions = (
+            _contains_non_overlapping_duplicate_sequence(value),
+            _contains_duplicate_with_buffer(value),
+        )
+        if all(conditions):
+            nice_strings += 1
+
+        if all(conditions) != bool(re.match(pat, value)):
+            status = "GOOD"
+        else:
+            status="\t\tBAD"
+        print(f"{value=}\t{status}")
+
+    return nice_strings
+
+
+if __name__ == '__main__':
+    data: list[str] = Path("../../../input.txt").read_text().strip().split("\n")
+
+    print(part_one(values=data))
+    print(part_two(values=data))
---
+++ events/advent_of_code/2015/6/implementations/original/python/script.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Event:
+    action: str
+    start_x: int
+    start_y: int
+    end_x: int
+    end_y: int
+
+    @classmethod
+    def from_str(cls, text):
+        text = text.replace("turn ", "")
+        texts = text.split(" ")
+
+        action = texts[0]
+        start_x, start_y = map(int, texts[1].split(","))
+        end_x, end_y = map(int, texts[3].split(","))
+
+        return cls(action, start_x, start_y, end_x, end_y)
+
+
+def part_one(events: list[Event]) -> int:
+    lit_lights = set()
+    for event in events:
+        for x in range(event.start_x, event.end_x + 1):
+            for y in range(event.start_y, event.end_y + 1):
+                match event.action:
+                    case "on":
+                        lit_lights.add((x, y))
+                    case "off":
+                        if (x, y) in lit_lights:
+                            lit_lights.remove((x, y))
+                    case "toggle":
+                        if (x, y) in lit_lights:
+                            lit_lights.remove((x, y))
+                        else:
+                            lit_lights.add((x, y))
+
+    return len(lit_lights)
+
+
+def part_two(events: list[Event]) -> int:
+    brightnesses: dict[tuple[int, int], int] = defaultdict(int)
+    for event in events:
+        for x in range(event.start_x, event.end_x + 1):
+            for y in range(event.start_y, event.end_y + 1):
+                match event.action:
+                    case "on":
+                        brightnesses[(x, y)] += 1
+                    case "off":
+                        brightnesses[(x, y)] -= 1
+                        if brightnesses[(x, y)] < 0:
+                            brightnesses[(x, y)] = 0
+                    case "toggle":
+                        brightnesses[(x, y)] += 2
+
+    return sum(brightnesses.values())
+
+
+if __name__ == '__main__':
+    data: list[str] = Path("../../../input.txt").read_text().strip().split("\n")
+    events_: list[Event] = [Event.from_str(item) for item in data]
+    print(events_[0])
+
+    print(part_one(events=events_))
+    print(part_two(events=events_))
