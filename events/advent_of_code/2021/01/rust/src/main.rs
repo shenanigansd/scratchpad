aa724e04550616ebfeff3ae6d6614c0c77dbf594
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-12-11 10:38:10 -0600

    aoc/2021/01: fix clippy lints
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- events/advent_of_code/2021/01/rust/src/main.rs
+++ events/advent_of_code/2021/01/rust/src/main.rs
@@ -8,7 +8,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn part_one(numbers: Vec<u16>) -> i32 {
+fn part_one(numbers: &[u16]) -> i32 {
     let mut count = 0;
     for (position, current_number) in numbers.iter().skip(1).enumerate() {
         let last_number = &numbers[position];
@@ -19,7 +19,7 @@ fn part_one(numbers: Vec<u16>) -> i32 {
     count
 }
 
-fn part_two(numbers: Vec<u16>) -> i32 {
+fn part_two(numbers: &[u16]) -> i32 {
     let mut sums: Vec<u16> = Vec::new();
     for (position, current_number) in numbers.iter().skip(2).enumerate() {
         sums.push(current_number + numbers[position] + numbers[position + 1]);
@@ -36,9 +36,9 @@ fn part_two(numbers: Vec<u16>) -> i32 {
 }
 
 fn main() {
-    let lines = lines_from_file("../../input.txt").expect("Could not load lines");
+    let lines = lines_from_file("events/advent_of_code/2021/01/input.txt").expect("Could not load lines");
     let numbers: Vec<u16> = lines.iter().map(|num| num.parse().unwrap()).collect();
 
-    println!("{}", part_one(numbers.clone()));
-    println!("{}", part_two(numbers.clone()));
+    println!("{}", part_one(&numbers));
+    println!("{}", part_two(&numbers));
 }
