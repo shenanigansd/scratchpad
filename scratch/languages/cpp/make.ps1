7ec02439b6592dd1c567f9bf47d746978ca37748
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-02-18 11:33:27 -0600

    pwshify the pwsh
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- scratch/languages/cpp/make.ps1
+++ scratch/languages/cpp/make.ps1
@@ -1,4 +1,25 @@
- mkdir build
- Set-Location build
- cmake ..
- cmake --build .
+<#
+.SYNOPSIS
+Run CMake
+
+.DESCRIPTION
+USAGE
+    .\make.ps1
+#>
+
+    [cmdletbinding()]
+Param()
+
+$path = "build"
+if (!(Test-Path -PathType Container -Path $path))
+{
+    New-Item -ItemType Directory -Path $path | Out-Null
+    Write-Verbose "created directory at $path"
+}
+else
+{
+    Write-Verbose "using existing directory at $path"
+}
+Set-Location -Path build
+cmake ..
+cmake --build .
