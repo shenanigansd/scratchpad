9221a45c3f7e3baf1d9fb1820d5bf04cd6b7c020
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-11-18 16:25:02 +0000

    Most of the double check wasn't interesting after all
    Signed-off-by: GitHub <noreply@github.com>

--- Gemfile
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-source 'https://rubygems.org'
-
-gem 'github-linguist', platforms: 'ruby'
-gem 'dotenv'
-gem 'rest-client'
--- doublecheckmeplease/utility.py
+++ doublecheckmeplease/utility.py
@@ -1,5 +1,10 @@
 import datetime
-
+import math
 
 def timestamp():
     return datetime.datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
+
+def get_design_folder(design_number):
+    thousand = str(math.trunc(design_number/1000))
+    thousand_folder = thousand + "000-" + thousand + "999"
+    return '\\\\IMPRESSDC\\IDInet\\graphics\\' + thousand_folder + '\\' + str(design_number)
