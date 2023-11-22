a0f11f35f0b065b2faf22e3f66a3df8fc1802551
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-11-22 16:03:29 +0000

    Add make scripts to atl
    Signed-off-by: GitHub <noreply@github.com>

---
+++ scratch/projects/dotnet/atl/fs/make.ps1
@@ -0,0 +1,70 @@
+<#
+.SYNOPSIS
+Make script for F#
+
+.DESCRIPTION
+USAGE
+    .\make.ps1 <command>
+
+COMMANDS
+    build      run `dotnet build`
+    run        run `dotnet run`
+    clean      delete generated content
+    help, -?   show this help message
+#>
+
+    [cmdletbinding()]
+param(
+    [Parameter(Position = 0)]
+    [ValidateSet("build", "run", "clean", "help")]
+    [string]$Command
+)
+
+function Invoke-Build
+{
+    dotnet build
+}
+
+function Invoke-Run
+{
+    dotnet run
+}
+
+function Invoke-Clean
+{
+    $folders = @("bin", "obj")
+    foreach ($folder in $folders)
+    {
+        if (Test-Path $folder)
+        {
+
+            Write-Verbose "Deleting $folder"
+            Remove-Item $folder -Recurse -Force
+        }
+    }
+}
+
+function Invoke-Help
+{
+    Get-Help $PSCommandPath
+}
+
+switch ($Command)
+{
+    "build"    {
+        Invoke-Build
+    }
+    "run"    {
+        Invoke-Run
+    }
+    "clean"    {
+        Invoke-Clean
+    }
+    "help"  {
+        Invoke-Help
+    }
+    default
+    {
+        Invoke-Build
+    }
+}
