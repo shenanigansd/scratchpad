b7ee17e59372d30ad5a4540e6ace71b7bf9813ab
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-12-15 00:31:09 +0000

    aoc/2023/07 -- closer to p2
    Signed-off-by: GitHub <noreply@github.com>

--- events/advent_of_code/2023/07/python/aoc_2023_07.py
+++ events/advent_of_code/2023/07/python/aoc_2023_07.py
@@ -7,6 +7,7 @@ from typing import Self
 STRENGTH_ORDER = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
 JOKER_STRENGTH_ORDER = ("J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A")
 
+
 class HandType(IntEnum):
     HIGH_CARD = 1
     ONE_PAIR = 2
@@ -25,14 +26,15 @@ class Hand:
     strength_order: tuple[str] = STRENGTH_ORDER
 
     @classmethod
-    def from_text(cls: type[Self], text: str, using_jokers:bool=False) -> Self:
+    def from_text(cls: type[Self], text: str, using_jokers: bool = False) -> Self:
         cards, bid = text.split()
         bid = int(bid)
         counter = dict(Counter(cards))
         if using_jokers:
             most_populus_card = max(counter, key=counter.get)
-            counter[most_populus_card] += counter.pop("J",0)
-        card_counts = sorted(Counter(cards).values(), reverse=True)
+            if most_populus_card != "J":
+                counter[most_populus_card] += counter.pop("J", 0)
+        card_counts = sorted(counter.values(), reverse=True)
         hand_type = None
         match card_counts:
             case [5]:
@@ -53,14 +55,14 @@ class Hand:
                 msg = f"Invalid hand: {text}"
                 raise ValueError(msg)
         strength_order = JOKER_STRENGTH_ORDER if using_jokers else STRENGTH_ORDER
-        return cls(cards=cards, bid=bid, type_=hand_type,strength_order=strength_order)
+        return cls(cards=cards, bid=bid, type_=hand_type, strength_order=strength_order)
 
     def __lt__(self: Self, other: Self) -> bool:
         if self.type_ < other.type_:
             return True
         if self.type_ > other.type_:
             return False
-        for self_card, other_card in zip(self.cards, other.cards):
+        for self_card, other_card in zip(self.cards, other.cards, strict=True):
             if self.strength_order.index(self_card) < self.strength_order.index(other_card):
                 return True
             if self.strength_order.index(self_card) > self.strength_order.index(other_card):
@@ -93,6 +95,6 @@ def part2(hands: list[Hand]) -> int:
 if __name__ == "__main__":
     raw_text = Path("../input.txt").read_text()
     hands = [Hand.from_text(line) for line in raw_text.split("\n") if line]
-    hands_but_with_jokers = [Hand.from_text(line,using_jokers=True) for line in raw_text.split("\n") if line]
+    hands_but_with_jokers = [Hand.from_text(line, using_jokers=True) for line in raw_text.split("\n") if line]
     print(part1(hands))
     print(part2(hands_but_with_jokers))
--- events/advent_of_code/2023/07/python/test_aoc_2023_07.py
+++ events/advent_of_code/2023/07/python/test_aoc_2023_07.py
@@ -1,5 +1,5 @@
 import pytest
-from aoc_2023_07 import Hand, HandType, part1,JOKER_STRENGTH_ORDER
+from aoc_2023_07 import JOKER_STRENGTH_ORDER, Hand, HandType, part1, part2
 
 HANDS = """
 32T3K 765
@@ -34,8 +34,9 @@ def test_sorts_hands_correctly() -> None:
         Hand(cards="QQQJA", type_=HandType.THREE_OF_A_KIND, bid=483),
     ]
 
+
 def test_sorts_joker_hands_correctly() -> None:
-    hands = [Hand.from_text(line,using_jokers=True) for line in HANDS.strip().split("\n")]
+    hands = [Hand.from_text(line, using_jokers=True) for line in HANDS.strip().split("\n")]
     assert sorted(hands) == [
         Hand(cards="32T3K", type_=HandType.ONE_PAIR, bid=765, strength_order=JOKER_STRENGTH_ORDER),
         Hand(cards="KK677", type_=HandType.TWO_PAIR, bid=28, strength_order=JOKER_STRENGTH_ORDER),
@@ -44,6 +45,12 @@ def test_sorts_joker_hands_correctly() -> None:
         Hand(cards="KTJJT", type_=HandType.FOUR_OF_A_KIND, bid=220, strength_order=JOKER_STRENGTH_ORDER),
     ]
 
+
 def test_part1_matches_sample_answer() -> None:
     hands = [Hand.from_text(line) for line in HANDS.strip().split("\n")]
     assert part1(hands) == 6440
+
+
+def test_part2_matches_sample_answer() -> None:
+    hands = [Hand.from_text(line, using_jokers=True) for line in HANDS.strip().split("\n")]
+    assert part2(hands) == 5905
