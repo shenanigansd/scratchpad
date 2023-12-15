b0729267c629c608ee7480ecbb3ca2f017643045
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-12-15 00:03:22 +0000

    aoc/2023/07 -- p2 WIP
    Signed-off-by: GitHub <noreply@github.com>

---
+++ events/advent_of_code/2023/07/python/aoc_2023_07.py
@@ -0,0 +1,98 @@
+from collections import Counter
+from dataclasses import dataclass
+from enum import IntEnum
+from pathlib import Path
+from typing import Self
+
+STRENGTH_ORDER = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+JOKER_STRENGTH_ORDER = ("J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A")
+
+class HandType(IntEnum):
+    HIGH_CARD = 1
+    ONE_PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    FULL_HOUSE = 5
+    FOUR_OF_A_KIND = 6
+    FIVE_OF_A_KIND = 7
+
+
+@dataclass(frozen=True)
+class Hand:
+    cards: str
+    type_: HandType
+    bid: int
+    strength_order: tuple[str] = STRENGTH_ORDER
+
+    @classmethod
+    def from_text(cls: type[Self], text: str, using_jokers:bool=False) -> Self:
+        cards, bid = text.split()
+        bid = int(bid)
+        counter = dict(Counter(cards))
+        if using_jokers:
+            most_populus_card = max(counter, key=counter.get)
+            counter[most_populus_card] += counter.pop("J",0)
+        card_counts = sorted(Counter(cards).values(), reverse=True)
+        hand_type = None
+        match card_counts:
+            case [5]:
+                hand_type = HandType.FIVE_OF_A_KIND
+            case [4, 1]:
+                hand_type = HandType.FOUR_OF_A_KIND
+            case [3, 2]:
+                hand_type = HandType.FULL_HOUSE
+            case [3, 1, 1]:
+                hand_type = HandType.THREE_OF_A_KIND
+            case [2, 2, 1]:
+                hand_type = HandType.TWO_PAIR
+            case [2, 1, 1, 1]:
+                hand_type = HandType.ONE_PAIR
+            case [1, 1, 1, 1, 1]:
+                hand_type = HandType.HIGH_CARD
+            case _:
+                msg = f"Invalid hand: {text}"
+                raise ValueError(msg)
+        strength_order = JOKER_STRENGTH_ORDER if using_jokers else STRENGTH_ORDER
+        return cls(cards=cards, bid=bid, type_=hand_type,strength_order=strength_order)
+
+    def __lt__(self: Self, other: Self) -> bool:
+        if self.type_ < other.type_:
+            return True
+        if self.type_ > other.type_:
+            return False
+        for self_card, other_card in zip(self.cards, other.cards):
+            if self.strength_order.index(self_card) < self.strength_order.index(other_card):
+                return True
+            if self.strength_order.index(self_card) > self.strength_order.index(other_card):
+                return False
+        msg = "Hands are equal"
+        raise ValueError(msg)
+
+    def __gt__(self: Self, other: Self) -> bool:
+        if self.type_ > other.type_:
+            return True
+        if self.type_ < other.type_:
+            return True
+        for self_card, other_card in zip(self.cards, other.cards, strict=True):
+            if self.strength_order.index(self_card) > self.strength_order.index(other_card):
+                return True
+            if self.strength_order.index(self_card) < self.strength_order.index(other_card):
+                return False
+        msg = "Hands are equal"
+        raise ValueError(msg)
+
+
+def part1(hands: list[Hand]) -> int:
+    return sum(hand.bid * index for index, hand in enumerate(sorted(hands), start=1))
+
+
+def part2(hands: list[Hand]) -> int:
+    return sum(hand.bid * index for index, hand in enumerate(sorted(hands), start=1))
+
+
+if __name__ == "__main__":
+    raw_text = Path("../input.txt").read_text()
+    hands = [Hand.from_text(line) for line in raw_text.split("\n") if line]
+    hands_but_with_jokers = [Hand.from_text(line,using_jokers=True) for line in raw_text.split("\n") if line]
+    print(part1(hands))
+    print(part2(hands_but_with_jokers))
---
+++ events/advent_of_code/2023/07/python/test_aoc_2023_07.py
@@ -0,0 +1,49 @@
+import pytest
+from aoc_2023_07 import Hand, HandType, part1,JOKER_STRENGTH_ORDER
+
+HANDS = """
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+"""
+
+
+@pytest.mark.parametrize(
+    ("text", "hand"),
+    [
+        ("32T3K 765", Hand(cards="32T3K", type_=HandType.ONE_PAIR, bid=765)),
+        ("T55J5 684", Hand(cards="T55J5", type_=HandType.THREE_OF_A_KIND, bid=684)),
+        ("KK677 28", Hand(cards="KK677", type_=HandType.TWO_PAIR, bid=28)),
+        ("KTJJT 220", Hand(cards="KTJJT", type_=HandType.TWO_PAIR, bid=220)),
+        ("QQQJA 483", Hand(cards="QQQJA", type_=HandType.THREE_OF_A_KIND, bid=483)),
+    ],
+)
+def test_can_parse_hand(text: str, hand: Hand) -> None:
+    assert Hand.from_text(text) == hand
+
+
+def test_sorts_hands_correctly() -> None:
+    hands = [Hand.from_text(line) for line in HANDS.strip().split("\n")]
+    assert sorted(hands) == [
+        Hand(cards="32T3K", type_=HandType.ONE_PAIR, bid=765),
+        Hand(cards="KTJJT", type_=HandType.TWO_PAIR, bid=220),
+        Hand(cards="KK677", type_=HandType.TWO_PAIR, bid=28),
+        Hand(cards="T55J5", type_=HandType.THREE_OF_A_KIND, bid=684),
+        Hand(cards="QQQJA", type_=HandType.THREE_OF_A_KIND, bid=483),
+    ]
+
+def test_sorts_joker_hands_correctly() -> None:
+    hands = [Hand.from_text(line,using_jokers=True) for line in HANDS.strip().split("\n")]
+    assert sorted(hands) == [
+        Hand(cards="32T3K", type_=HandType.ONE_PAIR, bid=765, strength_order=JOKER_STRENGTH_ORDER),
+        Hand(cards="KK677", type_=HandType.TWO_PAIR, bid=28, strength_order=JOKER_STRENGTH_ORDER),
+        Hand(cards="T55J5", type_=HandType.FOUR_OF_A_KIND, bid=684, strength_order=JOKER_STRENGTH_ORDER),
+        Hand(cards="QQQJA", type_=HandType.FOUR_OF_A_KIND, bid=483, strength_order=JOKER_STRENGTH_ORDER),
+        Hand(cards="KTJJT", type_=HandType.FOUR_OF_A_KIND, bid=220, strength_order=JOKER_STRENGTH_ORDER),
+    ]
+
+def test_part1_matches_sample_answer() -> None:
+    hands = [Hand.from_text(line) for line in HANDS.strip().split("\n")]
+    assert part1(hands) == 6440
