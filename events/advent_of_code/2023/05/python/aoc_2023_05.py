a369a00e69811d909d6c0cfa24ca1a32bc12bb59
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-02-25 04:04:32 +0000

    aoc/2023/05 -- use list of maps
    Signed-off-by: GitHub <noreply@github.com>

--- events/advent_of_code/2023/05/python/aoc_2023_05.py
+++ events/advent_of_code/2023/05/python/aoc_2023_05.py
@@ -1,55 +1,33 @@
 from dataclasses import dataclass
+from itertools import batched
 from pathlib import Path
-from darbia.utils.iterables import chunks
+
 
 @dataclass(frozen=True)
 class Almanac:
     seeds: list[int]
-
-    seed_to_soil_map: list[tuple[int, int, int]]
-    soil_to_fertilizer_map: list[tuple[int, int, int]]
-    fertilizer_to_water_map: list[tuple[int, int, int]]
-    water_to_light_map: list[tuple[int, int, int]]
-    light_to_temperature_map: list[tuple[int, int, int]]
-    temperature_to_humidity_map: list[tuple[int, int, int]]
-    humidity_to_location_map: list[tuple[int, int, int]]
+    maps: list[list[tuple[int, int, int]]]
 
     @classmethod
     def from_text(cls, text: str) -> "Almanac":
         lines = text.split("\n\n")
         seeds = [int(s) for s in lines[0].split(":")[1].split()]
-        seed_to_soil_map = [tuple(int(s) for s in line.split()) for line in lines[1].split("\n")[1:]]
-        soil_to_fertilizer_map = [tuple(int(s) for s in line.split()) for line in lines[2].split("\n")[1:]]
-        fertilizer_to_water_map = [tuple(int(s) for s in line.split()) for line in lines[3].split("\n")[1:]]
-        water_to_light_map = [tuple(int(s) for s in line.split()) for line in lines[4].split("\n")[1:]]
-        light_to_temperature_map = [tuple(int(s) for s in line.split()) for line in lines[5].split("\n")[1:]]
-        temperature_to_humidity_map = [tuple(int(s) for s in line.split()) for line in lines[6].split("\n")[1:]]
-        humidity_to_location_map = [tuple(int(s) for s in line.split()) for line in lines[7].split("\n")[1:] if line]
-        return cls(
-            seeds,
-            seed_to_soil_map,
-            soil_to_fertilizer_map,
-            fertilizer_to_water_map,
-            water_to_light_map,
-            light_to_temperature_map,
-            temperature_to_humidity_map,
-            humidity_to_location_map,
-        )
+
+        maps = []
+        for lines_group in lines[1:]:
+            titleless_lines = lines_group.split("\n")
+            titleless_lines.pop(0)
+            maps.append([
+                tuple(int(s) for s in line.split()) for line in titleless_lines if line
+            ])
+
+        return cls(seeds, maps)
 
     def smallest_seed_location(self) -> int:
         min_location = float("inf")
         for seed in self.seeds:
             location = seed
-            maps = [
-                self.seed_to_soil_map,
-                self.soil_to_fertilizer_map,
-                self.fertilizer_to_water_map,
-                self.water_to_light_map,
-                self.light_to_temperature_map,
-                self.temperature_to_humidity_map,
-                self.humidity_to_location_map,
-            ]
-            for map_ in maps:
+            for map_ in self.maps:
                 location = find_map_position(location, map_)
             if location < min_location:
                 min_location = location
@@ -57,23 +35,15 @@ class Almanac:
 
     def smallest_seed_range_location(self) -> int:
         min_location = float("inf")
-        for (start_seed, range_) in chunks(self.seeds, 2):
+        for start_seed, range_ in batched(self.seeds, 2):
             for seed in range(start_seed, start_seed + range_):
                 location = seed
-                maps = [
-                    self.seed_to_soil_map,
-                    self.soil_to_fertilizer_map,
-                    self.fertilizer_to_water_map,
-                    self.water_to_light_map,
-                    self.light_to_temperature_map,
-                    self.temperature_to_humidity_map,
-                    self.humidity_to_location_map,
-                ]
-                for map_ in maps:
+                for map_ in self.maps:
                     location = find_map_position(location, map_)
                 if location < min_location:
                     min_location = location
         return min_location
+
 
 def find_map_position(value: int, map_: list[tuple[int, int, int]]) -> int:
     for destination_start, source_start, range_ in map_:
--- events/advent_of_code/2023/05/python/test_aoc_2023_05.py
+++ events/advent_of_code/2023/05/python/test_aoc_2023_05.py
@@ -38,21 +38,25 @@ humidity-to-location map:
 
 TEST_ALMANAC = Almanac(
     seeds=[79, 14, 55, 13],
-    seed_to_soil_map=[(50, 98, 2), (52, 50, 48)],
-    soil_to_fertilizer_map=[(0, 15, 37), (37, 52, 2), (39, 0, 15)],
-    fertilizer_to_water_map=[(49, 53, 8), (0, 11, 42), (42, 0, 7), (57, 7, 4)],
-    water_to_light_map=[(88, 18, 7), (18, 25, 70)],
-    light_to_temperature_map=[(45, 77, 23), (81, 45, 19), (68, 64, 13)],
-    temperature_to_humidity_map=[(0, 69, 1), (1, 0, 69)],
-    humidity_to_location_map=[(60, 56, 37), (56, 93, 4)],
+    maps=[
+        [(50, 98, 2), (52, 50, 48)],
+        [(0, 15, 37), (37, 52, 2), (39, 0, 15)],
+        [(49, 53, 8), (0, 11, 42), (42, 0, 7), (57, 7, 4)],
+        [(88, 18, 7), (18, 25, 70)],
+        [(45, 77, 23), (81, 45, 19), (68, 64, 13)],
+        [(0, 69, 1), (1, 0, 69)],
+        [(60, 56, 37), (56, 93, 4)],
+    ],
 )
 
 
 def test_can_parse_almanac() -> None:
     assert Almanac.from_text(RAW_INPUT) == TEST_ALMANAC
 
+
 def test_can_find_smallest_location() -> None:
     assert TEST_ALMANAC.smallest_seed_location() == 35
+
 
 def test_can_find_smallest_range_location() -> None:
     assert TEST_ALMANAC.smallest_seed_range_location() == 46
