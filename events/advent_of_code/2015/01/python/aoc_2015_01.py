21329d00f6e1bb70867290bef5afb7b832152fa1
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-02-25 05:17:33 +0000

    aoc/2015/01 -- fixup
    Signed-off-by: GitHub <noreply@github.com>

--- events/advent_of_code/2015/01/python/aoc_2015_01.py
+++ events/advent_of_code/2015/01/python/aoc_2015_01.py
@@ -4,21 +4,30 @@ from pathlib import Path
 
 
 def part_one(text: str) -> int:
-    """Return the floor number (part one)."""
+    """Return the floor Santa ends up on.
+
+    All we need is the final floor, so we can just count the number of open and close parentheses.
+    """
     return sum(1 if char == "(" else -1 for char in text)
 
 
 def part_two(text: str) -> int:
-    """Return the position of the first character that causes Santa to enter the basement (part two)."""
+    """Return the position of the first character that causes Santa to enter the basement.
+
+    Now we need to keep track of the floor to know when Santa enters the basement.
+    """
     floor = 0
     for index, char in enumerate(text, start=1):
         if char == "(":
             floor += 1
         elif char == ")":
             floor -= 1
+
         if floor == -1:
             return index
-    return None
+
+    msg = "Santa never enters the basement!"
+    raise ValueError(msg)
 
 
 if __name__ == "__main__":
