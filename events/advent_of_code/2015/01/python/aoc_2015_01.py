9df86d80e580194bb788279ec6a43677371ee8f0
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-02-25 05:11:24 +0000

    aoc/2015/01 -- fixup
    Signed-off-by: GitHub <noreply@github.com>

--- events/advent_of_code/2015/01/python/aoc_2015_01.py
+++ events/advent_of_code/2015/01/python/aoc_2015_01.py
@@ -1,13 +1,17 @@
+"""Advent of Code 2015-01."""
+
 from pathlib import Path
 
 
 def part_one(text: str) -> int:
+    """Return the floor number (part one)."""
     return sum(1 if char == "(" else -1 for char in text)
 
 
 def part_two(text: str) -> int:
+    """Return the position of the first character that causes Santa to enter the basement (part two)."""
     floor = 0
-    for index, char in enumerate(text):
+    for index, char in enumerate(text, start=1):
         if char == "(":
             floor += 1
         elif char == ")":
@@ -18,7 +22,6 @@ def part_two(text: str) -> int:
 
 
 if __name__ == "__main__":
-    data: str = Path("../input.txt").read_text()[0].strip()
-
+    data = Path("../input.txt").read_text()
     print(part_one(text=data))
     print(part_two(text=data))
---
+++ events/advent_of_code/2015/01/python/test_aoc_2015_01.py
@@ -0,0 +1,31 @@
+import pytest
+from aoc_2015_01 import part_one, part_two
+
+
+@pytest.mark.parametrize(
+    ("text", "result"),
+    [
+        ("(())", 0),
+        ("()()", 0),
+        ("(((", 3),
+        ("(()(()(", 3),
+        ("))(((((", 3),
+        ("())", -1),
+        ("))(", -1),
+        (")))", -3),
+        (")())())", -3),
+    ],
+)
+def test_part_one(text: str, result: int) -> None:
+    assert part_one(text) == result
+
+
+@pytest.mark.parametrize(
+    ("text", "result"),
+    [
+        (")", 1),
+        ("()())", 5),
+    ],
+)
+def test_part_two(text: str, result: int) -> None:
+    assert part_two(text) == result
