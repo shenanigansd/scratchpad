9ac922fef35aa19776be424538826991ab6e9e12
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-12-03 02:28:04 +0000

    AOC 2024-02 broken part 2
    Signed-off-by: GitHub <noreply@github.com>

---
+++ events/advent_of_code/2024/02/python/aoc_2024_01.py
@@ -0,0 +1,71 @@
+import collections
+from dataclasses import dataclass
+from itertools import islice
+from pathlib import Path
+
+
+def sliding_window(iterable, n):
+    """Collect data into overlapping fixed-length chunks or blocks."""
+    # sliding_window('ABCDEFG', 4) → ABCD BCDE CDEF DEFG
+    iterator = iter(iterable)
+    window = collections.deque(islice(iterator, n - 1), maxlen=n)
+    for x in iterator:
+        window.append(x)
+        yield tuple(window)
+
+
+@dataclass(frozen=True)
+class Report:
+    values: list[int]
+
+    def is_safe(self, dampener_enabled: bool = False) -> bool:
+        dampener_used = False
+
+        increasing_changes = sum(1 for left, right in zip(self.values, self.values[1:]) if left > right)
+        print(f"{increasing_changes=}") 
+        if dampener_enabled and not dampener_used and increasing_changes == 1:
+            dampener_used = True
+            increasing_changes -= 1
+        all_increasing = increasing_changes == 0
+        print(f"{all_increasing=}") 
+
+        decreasing_changes = sum(1 for left, right in zip(self.values, self.values[1:]) if left < right)
+        print(f"{decreasing_changes=}")
+        if dampener_enabled and not dampener_used and decreasing_changes == 1:
+            dampener_used = True
+            decreasing_changes -= 1
+        all_decreasing = decreasing_changes == 0
+        print(f"{all_decreasing=}")
+
+        unsafe_values = 0
+        for left, right in sliding_window(self.values, 2):
+            print(f"{left=}, {right=}, {abs(left - right)=}")
+            if not 1 <= abs(left - right) <= 3:                                               
+                unsafe_values += 1
+        print(f"{unsafe_values=}")
+        if dampener_enabled and not dampener_used and unsafe_values == 1:
+            dampener_used = True
+            unsafe_values -= 1    
+        within_boundaries = unsafe_values == 0
+        print(f"{within_boundaries=}")
+
+        return (all_increasing or all_decreasing) and within_boundaries
+
+
+def parse_input(text: str) -> list[Report]:
+    reports = [list(map(int, row.split())) for row in text.strip().split("\n")]
+    return [Report(values) for values in reports]
+
+
+def part1(reports: list[Report]) -> int:
+    return sum(report.is_safe() for report in reports)
+
+
+def part2(reports: list[Report]) -> int:
+    return sum(report.is_safe(dampener_enabled=True) for report in reports)
+
+
+if __name__ == "__main__":
+    text = Path("../input.txt").read_text(encoding="locale")
+    print(part1(parse_input(text)))
+    print(part2(parse_input(text)))
---
+++ events/advent_of_code/2024/02/python/test_aoc_2024_01.py
@@ -0,0 +1,31 @@
+import pytest
+from aoc_2024_01 import Report
+
+
+@pytest.mark.parametrize(
+    ("values", "is_safe"),
+    [
+        ([7, 6, 4, 2, 1], True),
+        ([1, 2, 7, 8, 9], False),
+        ([9, 7, 6, 2, 1], False),
+        ([1, 3, 2, 4, 5], False),
+        ([8, 6, 4, 4, 1], False),
+        ([1, 3, 6, 7, 9], True),
+    ],
+)
+def test_report_is_safe(values: list[int], is_safe: bool) -> None:
+    assert Report(values).is_safe() == is_safe
+
+@pytest.mark.parametrize(
+    ("values", "is_safe"),
+    [
+        ([7, 6, 4, 2, 1], True),
+        ([1, 2, 7, 8, 9], False),
+        ([9, 7, 6, 2, 1], False),
+        ([1, 3, 2, 4, 5], True),
+        ([8, 6, 4, 4, 1], True),
+        ([1, 3, 6, 7, 9], True),
+    ],
+)
+def test_report_is_safe_wth_dampener(values: list[int], is_safe: bool) -> None:
+    assert Report(values).is_safe(dampener_enabled=True) == is_safe
