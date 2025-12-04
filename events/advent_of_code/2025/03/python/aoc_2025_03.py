3977c34f2236e3e089b64d66a91739f0dcf57c4b
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2025-12-04 01:52:09 +0000

    AOC 2025-03
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

---
+++ events/advent_of_code/2025/03/python/aoc_2025_03.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+
+def parse_input(text: str) -> list[list[int]]:
+    return [list(map(int, line)) for line in text.split("\n")]
+
+
+def part1(banks: list[list[int]]) -> int:
+    total = 0
+    for bank in banks:
+        biggest = max(bank[:-1])
+        next_biggest = max(bank[bank.index(biggest) + 1 :])
+        total += int(f"{biggest}{next_biggest}")
+    return total
+
+
+def part2(banks: list[list[int]]) -> int:
+    total = 0
+    for bank in banks:
+        while len(bank) > 12:
+            bank.remove(min(bank))
+        total += int("".join(map(str, bank)))
+    return total
+
+
+if __name__ == "__main__":
+    text = Path("../input.txt").read_text(encoding="locale")
+    values = parse_input(text)
+    print(part1(values))
+    print(part2(values))
